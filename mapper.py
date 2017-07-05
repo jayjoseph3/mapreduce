e24e0895bab710450c535bd3624cee8a3d434cc4
jayjoseph3 <root@centos7.localdomain>
2017-07-05 18:01:57 +0800

    mapreduce script

---
+++ mapper.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    unpacked = line.split(",")
+    stadium, capacity, expanded, location, surface, turf, team, opened, weather, roof, elevation = line.split(",")
+    results = [turf, "1"]
+    print("\t".join(results))
